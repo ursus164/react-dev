2df347fc0c98b64763be23297c21bcf22d301151
ursus164 <Galawieprzowa@gmail.com>
2024-08-21 12:56:27 +0200

    Added useState() hook in order to manage projects added by user, initial undefined state which causes NoProjectSelected component to render etc...

--- 08-project-management-app/src/App.jsx
+++ 08-project-management-app/src/App.jsx
@@ -4,17 +4,33 @@ import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
-  const [addProject, setAddProject] = useState(undefined)
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined, // property is set to undefined because it will be used to either store ID of the selected project, null - if we want to add new project, or undefined - if we don't want to add new project, or don't want to select any project either
+    projects:[] // projects created by user
+  })
 
-  function handleClick() {
-    setAddProject((addProject) => !addProject)
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,           // spreading previous state in order to not loose data from it - added projects etc...
+        selectedProjectId:null, // null - because we are adding some project right now
+      }
+    })
   }
+
+  let content 
+  if(projectsState.selectedProjectId === null) {
+    content = <NewProject/>
+  } else if(projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  } 
+
   return (
     // container which keeps all main components and data has a height based on screen width - it is like a container
     <main className="h-screen my-8 flex flex-row gap-8"> 
     {/* after adding flexbox to main elem, it automatically stretches the ProjectSidebar component so that it takes the whole height of the screen */}
-      <ProjectsSidebar onClick={handleClick}/>
-      {addProject ? <NewProject/> : <NoProjectSelected/>}
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
--- 08-project-management-app/src/components/ProjectsSidebar.jsx
+++ 08-project-management-app/src/components/ProjectsSidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button";
 
-export default function ProjectsSidebar({onClick}) {
+export default function ProjectsSidebar({onStartAddProject}) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200 text-center">
@@ -8,7 +8,7 @@ export default function ProjectsSidebar({onClick}) {
       </h2>
       <div className="text-center">
         {/* centering a button in div */}
-        <Button label='+ Add Project' onClick={onClick}/>
+        <Button label='+ Add Project' onClick={onStartAddProject}/>
       </div>
       {/* <ul>
                 {List of projects}
