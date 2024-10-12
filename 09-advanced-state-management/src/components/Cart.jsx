d810cbe573593c4ddb48f9703338c5208e3b1284
ursus164 <Galawieprzowa@gmail.com>
2024-10-12 12:01:38 +0200

    Migrating useState() to useReducer() hook in order to manage complex state in more flexible and elegant way

--- 09-advanced-state-management/src/components/Cart.jsx
+++ 09-advanced-state-management/src/components/Cart.jsx
@@ -4,6 +4,7 @@ import { CartContext } from "../store/shopping-cart-context";
 export default function Cart() {
   const { items, updateCartItemQuantity } = useContext(CartContext); // when we access context value in a component, and that value then changes - the component function that accesses that value, will get re-executed by react just as the component function would also get re-executed if we would be using some internal state that was updated or its parent compoent was executed again.
   const totalPrice = items.reduce(
+    // we are reducing the complex value - array of numbers into single value
     (acc, item) => acc + item.price * item.quantity,
     0
   );
