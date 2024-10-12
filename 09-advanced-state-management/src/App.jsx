db1a63105e7f6955425679600e1dbc2a949904be
ursus164 <Galawieprzowa@gmail.com>
2024-10-12 09:50:07 +0200

    Component embracing - App and Shop. One layer of prop drilling was removed

--- 09-advanced-state-management/src/App.jsx
+++ 09-advanced-state-management/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Product from './components/Product.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -72,7 +73,13 @@ function App() {
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+      <Shop onAddItemToCart={handleAddItemToCart}>
+      {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
     </>
   );
 }
