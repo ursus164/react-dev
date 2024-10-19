6dd36fae54e598bf9b65e8f543c6a50a088376b8
ursus164 <Galawieprzowa@gmail.com>
2024-10-19 10:21:20 +0200

    Updated modules numbering

--- 08-project-management-app/src/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-export default function Button({label, ...props}) {
-    return (
-        <button {...props} className="bg-stone-700 rounded-md py-2 px-4 text-xs md:text-base text-stone-400 hover:bg-stone-600 hover:text-stone-100">{label}</button>
-    )
-}
