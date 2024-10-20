a3f3aaaea02d46a8c835fab1d4891cb045ee9cc9
ursus164 <Galawieprzowa@gmail.com>
2024-10-20 12:25:00 +0200

    Added summary after the quiz which shows which answers were correct and which were wrong

--- 12-practice-project/src/components/Quiz.jsx
+++ 12-practice-project/src/components/Quiz.jsx
@@ -4,8 +4,8 @@
 import React, { useCallback } from "react";
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
-import quizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 export default function Quiz() {
   //-> this is redundant because we can derive the current active quesiton by checking number of answers in the user answers state below. If 2 answers are given, the next question shown should be the third one
@@ -54,10 +54,7 @@ export default function Quiz() {
 
   if (quizIsComplete) {
     return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Trophy Icon" />
-        <h2>Quiz Completed!</h2>
-      </div>
+      <Summary userAnswers={userAnswers}/>
     );
   }
 
