17c218f45b22df6113c54afb20a191d0a9775d21
ursus164 <Galawieprzowa@gmail.com>
2024-10-22 12:44:20 +0200

    Splitted login component into the StateLogin.jsx and Login.jsx. The approach was changed from managing inputs by state to managing them by refs - as an example of possible ways of doing the same thing

--- 17-forms-and-user-input/src/components/Login.jsx
+++ 17-forms-and-user-input/src/components/Login.jsx
@@ -1,33 +1,21 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Login() {
-  // const [enteredEmail, setEnteredEmail] = useState("");
-  // const [enteredPassword, setEnteredPassword] = useState("");
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
+  // second approach of handling user inputs is via refs - it requires less code than when using state. We did not add any value props, onChange etc...
 
-  // function handleEmailChange(event) {
-  //   // function will be triggered on every value change on the connected input (every keystroke for e.g)
-  //   setEnteredEmail(event.target.value);
-  // }
-
-  function handleInputChange(identifier, value) {
-    setEnteredValues((prevState) => ({
-      ...prevState, // rest of the values should not be lost
-      [identifier]: value, // dynamically accessing the name of the property when it is stored in the variale
-    }));
-  }
+  // Downside of this approach is that resetting those values below is a little bit harder.
+  // We should not do it directly -> email.current.value = ' ' - it is not recommended.
+  // Also we will have quite a lot of refs if we have a more complex form. So setting all refs and connecting them to DOM elements can take some time
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault(); // preventing default browser behaviour
 
-    console.log("Submitted"); // log is not visible because page is reloading after clicking login button. Also URL query parameters were added to the request. It is a problem because our server (which is a development server) is not prepared for dealing with that request that are automatically send on submission.
+    const enteredEmail = email.current.value; // current holds actual connected values
+    const enteredPassword = password.current.value;
 
-    // Also after we would deploy our app on some real server, that would be a server, that only aims to serve index.html file that is defined in the project - it still wouldn't be a server that is prepared to handle incoming form requests.
-
-    console.log("User email:" + enteredEmail);
+    console.log(enteredEmail,enteredPassword)
   }
 
   return (
@@ -38,32 +26,19 @@ export default function Login() {
         <div className="control no-margin">
           {/* htmlfor is rect equivalent for 'for' in native html */}
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={(event) => handleInputChange("email", event.target.value)}
-            value={enteredValues.email}
-          />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(event) => handleInputChange("password", event.target.value)}
-            value={enteredValues.password}
-          />
+          {/* By setting ref prop, connection will be established between DOM element below, and the ref above */}
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
       <p className="form-actions">
-        {/* Buttons inside a form will by default submit a form (generate a request), so HTTP requsest is created and is sent to the server that serves a website - that's a built in behaviour. So in our app it is a problem, unless we are using some full stack react solutions like NextJS.  */}
         <button className="button button-flat">Reset</button>
         <button className="button">Login</button>
-        {/* we can prevent that auto submitting by adding property type='button' */}
       </p>
     </form>
   );
---
+++ 17-forms-and-user-input/src/components/StateLogin.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  // function handleEmailChange(event) {
+  //   // function will be triggered on every value change on the connected input (every keystroke for e.g)
+  //   setEnteredEmail(event.target.value);
+  // }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevState) => ({
+      ...prevState, // rest of the values should not be lost
+      [identifier]: value, // dynamically accessing the name of the property when it is stored in the variale
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault(); // preventing default browser behaviour
+
+    console.log("Submitted"); // log is not visible because page is reloading after clicking login button. Also URL query parameters were added to the request. It is a problem because our server (which is a development server) is not prepared for dealing with that request that are automatically send on submission.
+
+    // Also after we would deploy our app on some real server, that would be a server, that only aims to serve index.html file that is defined in the project - it still wouldn't be a server that is prepared to handle incoming form requests.
+
+    console.log(enteredValues);
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          {/* htmlfor is rect equivalent for 'for' in native html */}
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange("password", event.target.value)}
+            value={enteredValues.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        {/* Buttons inside a form will by default submit a form (generate a request), so HTTP requsest is created and is sent to the server that serves a website - that's a built in behaviour. So in our app it is a problem, unless we are using some full stack react solutions like NextJS.  */}
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+        {/* we can prevent that auto submitting by adding property type='button' */}
+      </p>
+    </form>
+  );
+}
