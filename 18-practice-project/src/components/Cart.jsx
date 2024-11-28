8296731f1c83d5b2af5024e8d0e5d7678783ffc3
ursus164 <Galawieprzowa@gmail.com>
2024-11-28 17:03:48 +0100

    Added quantity modification to cart component and cartItem component

--- 18-practice-project/src/components/Cart.jsx
+++ 18-practice-project/src/components/Cart.jsx
@@ -1,30 +1,53 @@
-import Modal from "./ui/Modal"
-import CartContext from "../store/CartContext"
-import { useContext } from "react"
-import {currencyFormatter} from '../util/formatting'
-import Button from "./ui/Button"
-import UserProgressContext from "../store/UserProgressContext"
+import Modal from "./ui/Modal";
+import CartContext from "../store/CartContext";
+import { useContext } from "react";
+import { currencyFormatter } from "../util/formatting";
+import Button from "./ui/Button";
+import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 export default function Cart() {
-    const cartCtx = useContext(CartContext)
-    const userProgressCtx = useContext(UserProgressContext)
-    const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
-        totalPrice +=  item.quantity * item.price
-    }, 0);
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+  const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
+    totalPrice += item.quantity * item.price;
+    return totalPrice;
+  }, 0);
 
-    function handleCloseCart() {
-        userProgressCtx.hideCart();
-    }
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
 
-    return <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
-        <h2>Your Cart</h2>
-        <ul>
-            {cartCtx.items.map(item => <li key={item.id}>{item.name} - {item.quantity}</li>)}
-        </ul>
-        <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
-        <p className="modal-actions">
-            <Button textOnly onClick={handleCloseCart}>Close</Button>
-            <Button>Checkout</Button>
-        </p>
+  function increaseQuantity(item) {
+    cartCtx.addItem(item)
+}
+
+function decreaseQuantity(id) {
+    cartCtx.removeItem(id)
+}
+
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onDecrease={() => decreaseQuantity(item.id)}
+            onIncrease={() => increaseQuantity(item)}
+          />
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button>Checkout</Button>
+      </p>
     </Modal>
+  );
 }
