27a73379455389db8f75dd6dc66f96f1923c2da5
ursus164 <Galawieprzowa@gmail.com>
2024-11-29 19:58:44 +0100

    Added management of multiple pieces of data in reducer function in redux

--- 20-redux/src/components/Counter.js
+++ 20-redux/src/components/Counter.js
@@ -1,23 +1,38 @@
-import classes from './Counter.module.css';
-import { useSelector } from 'react-redux';
+import classes from "./Counter.module.css";
+import { useSelector,useDispatch } from "react-redux";
 
 const Counter = () => {
   // using useSelector will automatically set a subscription to redux store (behind the scenes)
-  const counter = useSelector(state => state.counter); //getting 'slice/part' of a state
-  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter); //getting 'slice/part' of a state -> possible because component is wrapped with provider
+  const show = useSelector(state => state.showCounter)
+  const dispatch = useDispatch();
 
-// const incrementHandler = () => {
+  const toggleCounterHandler = () => {
+    dispatch({type:'toggle'})
+  };
 
-// }
+  const incrementHandler = () => {
+    dispatch({type:'increment'});
+  };
+
+  const decrementHandler = () => {
+    dispatch({type:'decrement'})
+  };
+
+  const increaseHandler = () => {
+    dispatch({type: 'increase', amount: 5})
+  }
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      {/* <button onClick={incrementHandler}>Increment</button> */}
-      {/* <button onClick={decrementByFiveHandler}>Decrease by 5</button> */}
-      {/* <button onClick={decrementHandler}>Decrement</button> */}
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle counter</button>
-
     </main>
   );
 };
