918e8c34df6a73c15665a9d2d1fdc569218cde3b
ursus164 <Galawieprzowa@gmail.com>
2024-11-29 20:26:16 +0100

    Changed approach from editing state in immutable way in redux reducer to redux/toolkit method in which we use createSlice() hook.

--- 20-redux/src/store/index.js
+++ 20-redux/src/store/index.js
@@ -1,6 +1,25 @@
 import { createStore } from "redux";
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+    // preparation of slice of a global state
+    name: 'counter',
+    initialState: initialState,
+    reducers: {
+        increment(state) {
+            state.counter++; // it is now allowed - redux toolkit automatically detects code that mutates the state directly and it clones under the hood our state, create a new state object, keep all the state which we are not editing, and eventually overwrite settings which we want to change/edit - but in immutable way.
+        },
+        decrement(state) {state.counter--;},
+        increase(state,action) {
+            state.counter += action.amount; // muttable looking way but for the purpose above it is not muttable way of editing state :)
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
@@ -37,13 +56,5 @@ const counterReducer = (state = initialState, action) => {
 };
 
 const store = createStore(counterReducer);
-
-// const counterSubscriber = () => {
-//   const latestState = store.getState();
-// };
-
-// store.subscribe(counterSubscriber);
-// store.dispatch({ type: "increment" });
-// store.dispatch({ type: "decrement" });
 
 export default store;
