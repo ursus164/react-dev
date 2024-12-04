92ffafeee1ca87359a4597e6597f165789d7431f
ursus164 <Galawieprzowa@gmail.com>
2024-12-04 17:44:10 +0100

    Added notification component which triggers whether the data is being sent - it also shows error when something goes wrong

--- 21-advanced-redux/src/App.js
+++ 21-advanced-redux/src/App.js
@@ -1,26 +1,78 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { uiActions } from "./store/uiSlice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
-  const toggleCart = useSelector(state => state.ui.toggleCart);
-  const cart = useSelector(state => state.cart);
-  
+  const toggleCart = useSelector((state) => state.ui.toggleCart);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  const dispatch = useDispatch();
+
   // useEffect will listen to changes - whether some dependencies change(state updates...), the code will run again
   useEffect(() => {
-    fetch('https://react-redux-5e291-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {method: 'PUT',
-      body: JSON.stringify(cart)
-    }) // PUT method will overwrite the existing data instead of adding it to array (like it will be done with POST request in firebase)
-  },[cart])
-  
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending",
+          message: "Sending cart data...",
+        })
+      );
+      const response = await fetch(
+        "https://react-redux-5e291-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      ); // PUT method will overwrite the existing data instead of adding it to array (like it will be done with POST request in firebase)
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent cart data succesfully!",
+        })
+      );
+    };
+
+    if(isInitial) {
+      isInitial=false;
+      return;
+    }
+    
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {toggleCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {toggleCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- 21-advanced-redux/src/store/uiSlice.js
+++ 21-advanced-redux/src/store/uiSlice.js
@@ -1,17 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialUiState = { toggleCart: false};
+const initialUiState = { toggleCart: false, notification: null };
 
 const uiSlice = createSlice({
-    name:'ui',
-    initialState: initialUiState,
-    reducers: {
-        // map of actions representing methods in reducer
-        toggle(state) {
-            state.toggleCart = !state.toggleCart;
-        }
-    }
-})
+  name: "ui",
+  initialState: initialUiState,
+  reducers: {
+    // map of actions representing methods in reducer
+    toggle(state) {
+      state.toggleCart = !state.toggleCart;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status, // pending, waiting etc...
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
 
 export default uiSlice.reducer;
 export const uiActions = uiSlice.actions;
