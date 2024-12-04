3b03d4a2372298fd1e01804c8e9822976004107a
ursus164 <Galawieprzowa@gmail.com>
2024-12-04 17:14:04 +0100

    Set up new subscription to redux store by useSelector for whole cart. Whenever redux store does change this component function will be re executed and we will get the latest state - cart. Effect will also be re evaluated (whether cart changes).

--- 21-advanced-redux/src/App.js
+++ 21-advanced-redux/src/App.js
@@ -2,9 +2,19 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
-  const toggleCart = useSelector(state => state.ui.toggleCart)
+  const toggleCart = useSelector(state => state.ui.toggleCart);
+  const cart = useSelector(state => state.cart);
+  
+  // useEffect will listen to changes - whether some dependencies change(state updates...), the code will run again
+  useEffect(() => {
+    fetch('https://react-redux-5e291-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {method: 'PUT',
+      body: JSON.stringify(cart)
+    }) // PUT method will overwrite the existing data instead of adding it to array (like it will be done with POST request in firebase)
+  },[cart])
+  
 
   return (
     <Layout>
