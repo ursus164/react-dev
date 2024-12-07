7fe4622697f033eecc6a5d9f36d977b9501f2141
ursus164 <Galawieprzowa@gmail.com>
2024-12-07 12:17:50 +0100

    Added second route to the app and second possible solution of defining routes in router

--- 22-practice-project-with-router/src/App.js
+++ 22-practice-project-with-router/src/App.js
@@ -1,12 +1,28 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import HomePage from './pages/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 
 const router = createBrowserRouter([
-  {path: '/', element: <HomePage/> }
-])
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage/>} />
+//     <Route path="/products" element={<ProductsPage/>} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions)
 
 function App() {
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
