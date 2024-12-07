7b50ea5ea9fe45176413df89fa9d8f440bcac2e8
ursus164 <Galawieprzowa@gmail.com>
2024-12-07 12:29:21 +0100

    Implemented link elements for swithing pages without sendind unnecessary HTTP requests

--- 22-practice-project-with-router/src/pages/Home.js
+++ 22-practice-project-with-router/src/pages/Home.js
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom'
+import MainNavigation from '../components/MainNavigation';
+
+
 function HomePage() {
-    return <h1>My home page</h1>
+  return (
+    <>
+    <MainNavigation/>
+      <h1>My home page</h1>
+      {/* Link element prevents browser from sending unecessary HTTP request and lets react change view and URL without sendind http request */}
+      <p>
+        Go to <Link to="/products">list of products</Link>
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
