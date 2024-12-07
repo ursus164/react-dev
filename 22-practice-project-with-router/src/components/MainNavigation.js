94b5b7ff0798261a56dd5b5f4341040dd35b6ee0
ursus164 <Galawieprzowa@gmail.com>
2024-12-07 20:33:17 +0100

    Added current navigation link highliting.

--- 22-practice-project-with-router/src/components/MainNavigation.js
+++ 22-practice-project-with-router/src/components/MainNavigation.js
@@ -1,16 +1,32 @@
-import { Link } from "react-router-dom";
-import classes from './MainNavigation.module.css'
+import { NavLink } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <nav >
+      <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              // style={({ isActive }) => ({
+              //   textAlign: isActive ? "center" : "left",
+              // })}
+              end>
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }>
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
--- 22-practice-project-with-router/src/pages/Home.js
+++ 22-practice-project-with-router/src/pages/Home.js
@@ -1,6 +1,4 @@
 import { Link } from 'react-router-dom'
-import MainNavigation from '../components/MainNavigation';
-
 
 function HomePage() {
   return (
