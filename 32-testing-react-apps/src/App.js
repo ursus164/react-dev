94fdbdc7660b614181a85066c9dfb39070b6117b
ursus164 <Galawieprzowa@gmail.com>
2024-12-09 21:12:34 +0100

    Added async test for testing whether async data is being rendered after fetching

--- 32-testing-react-apps/src/App.js
+++ 32-testing-react-apps/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Greeting from "./components/Greeting";
+import Async from "./components/Async";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         </a>
       </header> */}
       <Greeting/>
+      <Async/>
     </div>
   );
 }
