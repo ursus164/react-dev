0cd1a48c129c9b202f9653dcd52b9dc7aa76fdb3
ursus164 <Galawieprzowa@gmail.com>
2024-12-09 19:46:49 +0100

    Added testing suite for Greeting element ;

--- 32-testing-react-apps/src/Greeting.test.js
+++ 32-testing-react-apps/src/Greeting.test.js
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./components/Greeting"
 
-test('renders hello world as a text', () => {
-    // Arange
-    render(<Greeting/>);
+describe('<Greeting/>', () => {
+    test('renders hello world as a text', () => {
+        // Arange
+        render(<Greeting/>);
+    
+        //Act 
+        // ..nothing
+    
+        // Assert - look into virtual dom
+        const helloWorldElement = screen.getByText('Hello World', { exact: false});
+        expect(helloWorldElement).toBeInTheDocument();
+    })
+});
 
-    //Act 
-    // ..nothing
 
-    // Assert - look into virtual dom
-    const helloWorldElement = screen.getByText('Hello World', { exact: false});
-    expect(helloWorldElement).toBeInTheDocument();
-})
